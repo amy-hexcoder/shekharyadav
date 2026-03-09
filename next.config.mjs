e117ba9c988acbdd2bd0d80c9da0cbfffd65a01e
amy-hexcoder <amywilcox237@gmail.com>
2026-03-08 23:43:46 -0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// next.config.mjs
+import { withContentlayer } from 'next-contentlayer';
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    mdxRs: true,
+  },
+};
+
+export default withContentlayer(nextConfig);
