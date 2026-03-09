96cc65d6ce236d5dfe3625cc872318487a9fb101
amy-hexcoder <amywilcox237@gmail.com>
2026-03-09 00:29:25 -0700

    Create MDXContent.tsx

---
+++ components/MDXContent.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useMDXComponent } from 'next-contentlayer2/hooks'
+import MDXComponents from '@/components/MDXComponents'
+
+type Props = {
+  code: string
+}
+
+export default function MDXContent({ code }: Props) {
+  const Component = useMDXComponent(code)
+
+  return (
+    <MDXComponents>
+      <Component />
+    </MDXComponents>
+  )
+}
