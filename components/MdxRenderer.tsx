673d33b18a4940fe0bc570b12ad78ea9da53c9ae
amy-hexcoder <amywilcox237@gmail.com>
2026-03-10 01:50:28 -0700

    Update MdxRenderer.tsx

--- components/MdxRenderer.tsx
+++ components/MdxRenderer.tsx
@@ -4,13 +4,7 @@
 import { useMDXComponent } from 'next-contentlayer2/hooks'; // or '@contentlayer2/next/hooks'
 import { mdxComponents } from '@/components/mdx-components.client';
 
-export function MdxRenderer({
-  code,
-  components,
-}: {
-  code: string;
-  components?: mdxComponents;
-}) {
+export function MdxRenderer({ code }: { code: string }) {
   const Component = useMDXComponent(code);
-  return <Component components={components} />;
+  return <Component components={mdxComponents} />;
 }
