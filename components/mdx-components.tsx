9b206cfdba01057bca876dec7ec441552a401d1c
amy-hexcoder <amywilcox237@gmail.com>
2026-03-10 00:50:00 -0700

    Create mdx-components.tsx

---
+++ components/mdx-components.tsx
@@ -0,0 +1,9 @@
+// mdx-components.tsx
+import { Callout } from '@/components/Callout';
+
+export const mdxComponents = {
+  Callout,
+  // You can add more custom components here
+  // Image: CustomImage,
+  // CodeBlock: CustomCodeBlock,
+};
