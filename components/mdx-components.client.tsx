a8ecb64aef8b3b52588222a65a1e94c48b80dd70
amy-hexcoder <amywilcox237@gmail.com>
2026-03-10 01:46:23 -0700

    Create mdx-components.client.tsx

---
+++ components/mdx-components.client.tsx
@@ -0,0 +1,9 @@
+// components/mdx-components.client.tsx
+'use client';
+
+import type { MDXComponents } from 'mdx/types';
+import { Callout } from '@/components/Callout';
+
+export const mdxComponents: MDXComponents = {
+  Callout
+};
