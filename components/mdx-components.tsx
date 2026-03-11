a08026ffd01fd1776f21284c6ddecbaea6b7dbbb
amy-hexcoder <amywilcox237@gmail.com>
2026-03-11 00:41:21 -0700

    Update mdx-components.tsx

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,8 +1,11 @@
 // mdx-components.tsx
 import { Callout } from '@/components/Callout';
+import Image from 'next/image';
+
 
 export const mdxComponents = {
   Callout,
+  Image
   // You can add more custom components here
   // Image: CustomImage,
   // CodeBlock: CustomCodeBlock,
